6d855abfd6f31c416fef02db15acb55e87fd75d4
evandro miranda <evandrogmiranda@hotmail.com>
2023-02-28 15:23:30 -0300

    Componentes

---
+++ blog-pessoal/src/componentes/home/Home.tsx
@@ -0,0 +1,13 @@
+import react from "react";
+import "./Home.css";
+
+function home(){
+    return(
+        <>
+            <h1 className="titulo">Olá meu nome é Evandro</h1>
+            <img src="https://imgur.com/tl9D4L8.png" alt="imagem inicial" className="img"/>
+        </>
+    );
+}
+
+export default home;
